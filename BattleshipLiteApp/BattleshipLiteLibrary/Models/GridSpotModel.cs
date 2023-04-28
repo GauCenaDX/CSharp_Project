15f60dfecc8054cf00c5ed546cfbb01238cb6102
Duc Nguyen <ducnguyen0305@gmail.com>
2023-04-27 23:43:57 -0700

    Project Battleship Lite App - Create Class Library

---
+++ BattleshipLiteApp/BattleshipLiteLibrary/Models/GridSpotModel.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System;
+namespace BattleshipLiteLibrary.Models
+{
+	public class GridSpotModel
+	{
+		public string SpotLetter { get; set; }
+		public int SpotNumber { get; set; }
+        //-- Assign original value to Auto Property
+        public GridSpotStatus Status { get; set; } = GridSpotStatus.Empty;
+	}
+}
+
---
+++ BattleshipLiteApp/BattleshipLiteLibrary/Models/PlayerInfoModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System;
+namespace BattleshipLiteLibrary.Models
+{
+	public class PlayerInfoModel
+	{
+		public string PlayerName { get; set; }
+		public List<GridSpotModel> ShipLocations { get; set; }
+		public List<GridSpotModel> ShotGrid { get; set; }
+	}
+}
+
