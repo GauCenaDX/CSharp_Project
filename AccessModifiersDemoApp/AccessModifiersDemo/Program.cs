2e2410213324f30a50a45afadbad9e5f15f8aeba
Duc Nguyen <ducnguyen0305@gmail.com>
2023-05-11 23:23:17 -0700

    Access Modifiers
    - Public
    - Private
    - Protected
    - Internal
    - Protected Internal
    - Private Protected

---
+++ AccessModifiersDemoApp/AccessModifiersDemo/Program.cs
@@ -0,0 +1,78 @@
+﻿
+using System;
+using DemoLibrary;
+
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/access-modifiers
+
+namespace AccessModifiersDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            // Only have access to
+            person.FirstName = "Zoro";
+            person.SayHello();
+            person.SSN = "123456789";
+            Console.WriteLine(person.SSN);
+
+            // Only way to change this person last name
+            person.ChangeLastName("Luffy");
+            Console.WriteLine($"This person last name is {person.LastName} now.");
+
+
+            // Employee inherited from Person, Manager inherited from Employee.
+            // Both Employee and Manager have access to Protected entity in Person.
+            Employee employee = new Employee();
+            Manager manager = new Manager();
+
+            employee.FirstName = "John";
+            employee.ChangeLastName("Doe");
+            manager.FirstName = "Mama";
+            manager.ChangeLastName("Mamma");
+            Console.WriteLine();
+            Console.WriteLine($"Former last name: {employee.GetFormerLastName()}");
+            Console.WriteLine($"All names: {manager.GetAllNames()}");
+
+
+            //DataAccess data = new DataAccess();
+            //Console.WriteLine();
+            //Console.WriteLine(data.GetConnectionString());
+            person.SavePerson();
+
+
+            Console.WriteLine();
+            Console.WriteLine("End of Program. Hit Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+
+    public class ModifiedDataAccess : DataAccess
+    {
+        public void GetUnsecureConnectionInfo()
+        {
+            GetConnectionString();
+        }
+    }
+
+    public class CEO : Manager
+    {
+        public void GetConnectionInfo()
+        {
+            // Inaccessible due to its protection level
+            //DataAccess data = new DataAccess();
+            //string conn = data.GetConnectionString();
+
+            formerLastName = "Origin";
+
+
+            ModifiedDataAccess data = new ModifiedDataAccess();
+            data.GetUnsecureConnectionInfo();
+
+            // No direct access to nickName
+            GetNickName();
+        }
+    }
+}
---
+++ AccessModifiersDemoApp/DemoLibrary/Person.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+
+namespace DemoLibrary
+{
+    //internal class DataAccess
+    //{
+    //    internal string GetConnectionString()
+    //    {
+	//		  return "Sensitive data";
+	//	  }
+	//}
+
+    public class DataAccess
+    {
+        protected internal string GetConnectionString()
+        {
+            return "Sensitive data";
+        }
+    }
+
+    public class Person
+	{
+		//private string formerLastName = "";
+		protected string formerLastName = "";
+		private protected string nickName = "PrivateProtected";
+
+        public string FirstName { get; set; }
+		public string LastName { get; private set; }
+
+		private string _ssn;
+
+		public string SSN
+		{
+			get
+			{
+				return $"xxx-xx-{_ssn.Substring(_ssn.Length - 4)}";
+			}
+
+			set
+			{
+				_ssn = value;
+			}
+		}
+
+		public void ChangeLastName(string newLastName)
+		{
+			formerLastName = LastName;
+			LastName = newLastName;
+		}
+
+		public void SayHello()
+		{
+			Console.WriteLine($"Hello {FirstName}");
+		}
+
+		public void SavePerson()
+		{
+			DataAccess data = new DataAccess();
+			string conn = data.GetConnectionString();
+			// Saves a person
+		}
+	}
+
+	public class Employee : Person
+	{
+		public string GetFormerLastName()
+		{
+			return formerLastName;
+		}
+
+		public string GetNickName()
+		{
+			return nickName;
+		}
+	}
+
+	public class Manager : Employee
+	{
+		public string GetAllNames()
+		{
+			return $"{FirstName}, {LastName}, {formerLastName}";
+		}
+	}
+}
+
