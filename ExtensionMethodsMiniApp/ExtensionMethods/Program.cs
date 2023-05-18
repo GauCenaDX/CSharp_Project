54dca73b3311e4af64b74cb694320f3cae6fcc93
Duc Nguyen <ducnguyen0305@gmail.com>
2023-05-17 23:18:20 -0700

    Mini Project: Extension Methods
    - Method overloading
    - Extension methods

---
+++ ExtensionMethodsMiniApp/ExtensionMethods/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+
+            person.FirstName = "What is your first name: ".RequestString();
+            person.LastName = "What is your last name: ".RequestString();
+            person.Age = "What is your age: ".RequestInt(0, 120);
+            //person.Age = ConsoleHelper.RequestInt("What is your age: ", 0, 120);
+
+            Console.WriteLine();
+            Console.WriteLine("End of program. Hit Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
