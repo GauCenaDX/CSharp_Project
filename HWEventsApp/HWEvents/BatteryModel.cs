ce010d44053f3a74ae8196dc97b7a43c5f5082c2
Duc Nguyen <ducnguyen0305@gmail.com>
2023-05-22 22:15:18 -0700

    Homework on Events

---
+++ HWEventsApp/HWEvents/BatteryModel.cs
@@ -0,0 +1,26 @@
+﻿
+namespace HWEvents
+{
+    public class BatteryModel
+    {
+        public event EventHandler<string> BatteryIsLow;
+
+        public int BatteryLevel { get; private set; } = 100;
+
+        public void DrainBatteryLevel(int percentage)
+        {
+            BatteryLevel -= percentage;
+        }
+
+        public void CheckBatteryLevel()
+        {
+            Console.WriteLine($"Battery level is at {BatteryLevel}%");
+
+            if (BatteryLevel < 25)
+            {
+                BatteryIsLow?.Invoke(this, $"WARNING: Battery is under 25%.");
+            }
+
+        }
+    }
+}
---
+++ HWEventsApp/HWEvents/Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace HWEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BatteryModel battery = new BatteryModel();
+
+            battery.BatteryIsLow += Battery_BatteryIsLow;
+
+            while (battery.BatteryLevel > 0)
+            {
+                battery.CheckBatteryLevel();
+                battery.DrainBatteryLevel(1);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("End of program. Hit Enter to exit.");
+            Console.ReadLine();
+        }
+
+        private static void Battery_BatteryIsLow(object sender, string e)
+        {
+            Console.WriteLine();
+            Console.WriteLine(e);
+            Console.WriteLine();
+        }
+    }
+}
