404e3ca605dd7dec80d3a54c2af5b49f19283031
Duc Nguyen <ducnguyen0305@gmail.com>
2023-05-29 22:31:44 -0700

    Class Libraries Project Types

---
+++ ClassLibraryDemoApp/ConsoleUI/Program.cs
@@ -0,0 +1,31 @@
+﻿
+//using FrameworkLibrary;
+//using CoreLibrary;
+using StandardLibrary;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generators generators = new Generators();
+            PersonModel person = new PersonModel
+            {
+                Prefix = "Mr.",
+                FirstName = "John",
+                LastName = "Snow"
+            };
+
+            string message = generators.WelcomeMessage(person.Prefix, person.LastName);
+            Console.WriteLine(message);
+
+            Console.WriteLine();
+            message = generators.FarewellMessage(person.Prefix, person.LastName);
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+        }
+    }
+}
