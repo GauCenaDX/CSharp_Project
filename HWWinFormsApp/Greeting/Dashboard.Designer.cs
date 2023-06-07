bed129877dbca10196a48918005a2b2e7cd2cbcc
Duc Nguyen <ducnguyen0305@gmail.com>
2023-06-07 00:10:52 -0700

    Homework on WinForms - Greeting

---
+++ HWWinFormsApp/Greeting/Dashboard.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Greeting
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.sayHiButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(44, 40);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(131, 29);
+            this.firstNameLabel.TabIndex = 0;
+            this.firstNameLabel.Text = "First Name";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(265, 40);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(128, 29);
+            this.lastNameLabel.TabIndex = 0;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(49, 84);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(195, 35);
+            this.firstNameTextBox.TabIndex = 1;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(270, 84);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(195, 35);
+            this.lastNameTextBox.TabIndex = 2;
+            // 
+            // sayHiButton
+            // 
+            this.sayHiButton.Location = new System.Drawing.Point(356, 143);
+            this.sayHiButton.Name = "sayHiButton";
+            this.sayHiButton.Size = new System.Drawing.Size(109, 35);
+            this.sayHiButton.TabIndex = 3;
+            this.sayHiButton.Text = "Say Hi";
+            this.sayHiButton.UseVisualStyleBackColor = true;
+            this.sayHiButton.Click += new System.EventHandler(this.sayHiButton_Click);
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(519, 247);
+            this.Controls.Add(this.sayHiButton);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "Dashboard";
+            this.Text = "Greeting Someone";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.Button sayHiButton;
+    }
+}
+
---
+++ HWWinFormsApp/Greeting/Dashboard.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Greeting
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void sayHiButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(firstNameTextBox.Text))
+            {
+                PromptUserInput(firstNameTextBox, firstNameLabel);
+            }
+            else if (string.IsNullOrWhiteSpace(lastNameTextBox.Text))
+            {
+                PromptUserInput(lastNameTextBox, lastNameLabel);
+            }
+            else
+            {
+                MessageBox.Show($"Hi {firstNameTextBox.Text} {lastNameTextBox.Text}");
+                firstNameTextBox.Text = "";
+                lastNameTextBox.Text = "";
+                firstNameTextBox.Focus();
+            }
+        }
+
+        public void PromptUserInput(TextBox textBox, Label label)
+        {
+            MessageBox.Show($"Please enter information for {label.Text} field.",
+                "Blank Field Detected", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            textBox.Text = "";
+            textBox.Focus();
+        }
+    }
+}
