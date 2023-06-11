35580a6922209bd6eb65e653e84edd8f2813c58f
Duc Nguyen <ducnguyen0305@gmail.com>
2023-06-11 16:25:56 -0700

    Homework on WinForms - WinFormMiniProjectApp - Instructor Version

---
+++ WinFormMiniProjectApp/WinFormMiniProject/AddressEntry.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WinFormMiniProject
+{
+    partial class AddressEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cityText = new System.Windows.Forms.TextBox();
+            this.cityLabel = new System.Windows.Forms.Label();
+            this.streetAddressText = new System.Windows.Forms.TextBox();
+            this.streetAddressLabel = new System.Windows.Forms.Label();
+            this.stateLabel = new System.Windows.Forms.Label();
+            this.stateText = new System.Windows.Forms.TextBox();
+            this.zipCodeLabel = new System.Windows.Forms.Label();
+            this.zipCodeText = new System.Windows.Forms.TextBox();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.saveRecord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cityText
+            // 
+            this.cityText.Location = new System.Drawing.Point(228, 158);
+            this.cityText.Name = "cityText";
+            this.cityText.Size = new System.Drawing.Size(315, 35);
+            this.cityText.TabIndex = 2;
+            // 
+            // cityLabel
+            // 
+            this.cityLabel.AutoSize = true;
+            this.cityLabel.Location = new System.Drawing.Point(162, 161);
+            this.cityLabel.Name = "cityLabel";
+            this.cityLabel.Size = new System.Drawing.Size(53, 29);
+            this.cityLabel.TabIndex = 3;
+            this.cityLabel.Text = "City";
+            // 
+            // streetAddressText
+            // 
+            this.streetAddressText.Location = new System.Drawing.Point(228, 102);
+            this.streetAddressText.Name = "streetAddressText";
+            this.streetAddressText.Size = new System.Drawing.Size(315, 35);
+            this.streetAddressText.TabIndex = 1;
+            // 
+            // streetAddressLabel
+            // 
+            this.streetAddressLabel.AutoSize = true;
+            this.streetAddressLabel.Location = new System.Drawing.Point(43, 105);
+            this.streetAddressLabel.Name = "streetAddressLabel";
+            this.streetAddressLabel.Size = new System.Drawing.Size(172, 29);
+            this.streetAddressLabel.TabIndex = 4;
+            this.streetAddressLabel.Text = "Street Address";
+            // 
+            // stateLabel
+            // 
+            this.stateLabel.AutoSize = true;
+            this.stateLabel.Location = new System.Drawing.Point(147, 220);
+            this.stateLabel.Name = "stateLabel";
+            this.stateLabel.Size = new System.Drawing.Size(68, 29);
+            this.stateLabel.TabIndex = 4;
+            this.stateLabel.Text = "State";
+            // 
+            // stateText
+            // 
+            this.stateText.Location = new System.Drawing.Point(228, 217);
+            this.stateText.Name = "stateText";
+            this.stateText.Size = new System.Drawing.Size(315, 35);
+            this.stateText.TabIndex = 3;
+            // 
+            // zipCodeLabel
+            // 
+            this.zipCodeLabel.AutoSize = true;
+            this.zipCodeLabel.Location = new System.Drawing.Point(103, 276);
+            this.zipCodeLabel.Name = "zipCodeLabel";
+            this.zipCodeLabel.Size = new System.Drawing.Size(112, 29);
+            this.zipCodeLabel.TabIndex = 3;
+            this.zipCodeLabel.Text = "Zip Code";
+            // 
+            // zipCodeText
+            // 
+            this.zipCodeText.Location = new System.Drawing.Point(228, 273);
+            this.zipCodeText.Name = "zipCodeText";
+            this.zipCodeText.Size = new System.Drawing.Size(315, 35);
+            this.zipCodeText.TabIndex = 4;
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.Location = new System.Drawing.Point(160, 30);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(249, 39);
+            this.headerLabel.TabIndex = 7;
+            this.headerLabel.Text = "Address Entry";
+            // 
+            // saveRecord
+            // 
+            this.saveRecord.Location = new System.Drawing.Point(228, 334);
+            this.saveRecord.Name = "saveRecord";
+            this.saveRecord.Size = new System.Drawing.Size(133, 42);
+            this.saveRecord.TabIndex = 5;
+            this.saveRecord.Text = "Save";
+            this.saveRecord.UseVisualStyleBackColor = true;
+            this.saveRecord.Click += new System.EventHandler(this.saveRecord_Click);
+            // 
+            // AddressEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 411);
+            this.Controls.Add(this.saveRecord);
+            this.Controls.Add(this.headerLabel);
+            this.Controls.Add(this.zipCodeText);
+            this.Controls.Add(this.cityText);
+            this.Controls.Add(this.zipCodeLabel);
+            this.Controls.Add(this.cityLabel);
+            this.Controls.Add(this.stateText);
+            this.Controls.Add(this.stateLabel);
+            this.Controls.Add(this.streetAddressText);
+            this.Controls.Add(this.streetAddressLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "AddressEntry";
+            this.Text = "Address Entry Form by Tim Corey";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox cityText;
+        private System.Windows.Forms.Label cityLabel;
+        private System.Windows.Forms.TextBox streetAddressText;
+        private System.Windows.Forms.Label streetAddressLabel;
+        private System.Windows.Forms.Label stateLabel;
+        private System.Windows.Forms.TextBox stateText;
+        private System.Windows.Forms.Label zipCodeLabel;
+        private System.Windows.Forms.TextBox zipCodeText;
+        private System.Windows.Forms.Label headerLabel;
+        private System.Windows.Forms.Button saveRecord;
+    }
+}
---
+++ WinFormMiniProjectApp/WinFormMiniProject/PersonEntry.cs
@@ -0,0 +1,56 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WinFormMiniProject
+{
+    public partial class PersonEntry : Form, ISaveAddress
+    {
+        BindingList<AddressModel> addresses = new BindingList<AddressModel>();
+
+        public PersonEntry()
+        {
+            InitializeComponent();
+
+            //addresses.Add(new AddressModel { StreetAddress = "123 State Street", City = "Scranton", State = "CA", ZipCode = "12345" });
+
+            // WinForm will display the ToString() of an object ==> specify how object should be displayed
+            addressesList.DataSource = addresses;
+            addressesList.DisplayMember = nameof(AddressModel.AddressDisplayValue);
+        }
+
+        private void addNewAddress_Click(object sender, EventArgs e)
+        {
+            // Open the Address Entry form
+            AddressEntry entry = new AddressEntry(this);
+
+            entry.Show();
+        }
+
+        public void SaveAddress(AddressModel address)
+        {
+            addresses.Add(address);
+        }
+
+        private void saveRecord_Click(object sender, EventArgs e)
+        {
+            PersonModel person = new PersonModel
+            {
+                FirstName = firstNameText.Text,
+                LastName = lastNameText.Text,
+                IsActive = isActive.Checked, //-- bool CheckBox.Checked { get; set; }
+                Addresses = addresses.ToList()
+            };
+
+            // We can then do something with this person object
+        }
+    }
+}
