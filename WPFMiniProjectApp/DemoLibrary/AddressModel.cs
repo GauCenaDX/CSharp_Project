df6c3458dc4da2917d4a02365437cfce9baaec36
Duc Nguyen <ducnguyen0305@gmail.com>
2023-06-27 22:57:01 -0700

    Homework on WPF - WPFMiniProjectApp - Instructor Version

---
+++ WPFMiniProjectApp/DemoLibrary/AddressModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoLibrary
+{
+    public class AddressModel
+    {
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        // Create a full property (read-only) using Lambda
+        public string AddressDisplayValue => $"{StreetAddress}, {City}, {State}  {ZipCode}";
+
+        //public string FullAddressView
+        //{
+        //    get { return $"{StreetAddress}, {City}, {State} {ZipCode}"; }
+        //}
+    }
+}
---
+++ WPFMiniProjectApp/WPFMiniProject/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMiniProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, ISaveAddress
+    {
+        BindingList<AddressModel> addresses = new BindingList<AddressModel>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            addressesList.ItemsSource = addresses;
+
+            //addresses.Add(new AddressModel
+            //{ 
+            //    StreetAddress = "123 Wallabee Way",
+            //    City = "Clarks Summit",
+            //    State = "PA",
+            //    ZipCode = "18411"
+            //});
+        }
+
+        public void SaveAddress(AddressModel address)
+        {
+            addresses.Add(address);
+        }
+
+        private void AddAddress_Click(object sender, RoutedEventArgs e)
+        {
+            AddressEntry entry = new AddressEntry(this);
+
+            entry.Show();
+        }
+
+        private void SavePerson_Click(object sender, RoutedEventArgs e)
+        {
+            PersonModel person = new PersonModel
+            {
+                FirstName = firstNameText.Text,
+                LastName = lastNameText.Text,
+                IsActive = (activeCheckBox.IsChecked ?? false), //-- null coalescing operator
+                Addresses = addresses.ToList()
+            };
+        }
+    }
+}
