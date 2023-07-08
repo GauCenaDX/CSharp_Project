bd867132831b583feaf2373214abb9f5cb671307
Duc Nguyen <ducnguyen0305@gmail.com>
2023-07-08 16:00:49 -0700

    YOC - Week17 - BrokenAppDemo

---
+++ BrokenAppDemo/BrokenLibrary/CompanyModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BrokenLibrary
+{
+    class CompanyModel
+    {
+        public string CompanyName { get; set; }
+        public int NumberOfEmployees { get; set; }
+        public int NumberOfDaysOffTotal { get; set; }
+        
+        int AverageDaysOff ()
+        {
+            return NumberOfDaysOffTotal / NumberOfEmployees;
+        }
+    }
+}
---
+++ BrokenAppDemo/Readme.txt
@@ -0,0 +1,7 @@
+
+
+- Debug an application that is currently not working.
+- Fix the bugs and ensure that the application is working correctly.
+
+Note: the logic is correct so there is no missing lines of code.
+
