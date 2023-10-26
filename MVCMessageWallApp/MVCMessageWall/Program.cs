cb3c6bf27e0ef7c4826f333383818b92fc965fba
Duc Nguyen <ducnguyen0305@gmail.com>
2023-10-25 22:07:34 -0700

    ASP.NET Core MVC - MVCMessageWall - Completed
    Note - Unable to setup Authentication.

--- MVCMessageWallApp/MVCMessageWall/Program.cs
+++ MVCMessageWallApp/MVCMessageWall/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MVCMessageWall.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("MVCMessageWallContextConnection") ?? throw new InvalidOperationException("Connection string 'MVCMessageWallContextConnection' not found.");
+
+builder.Services.AddDbContext<MVCMessageWallContext>(options =>
+    options.UseSqlServer(connectionString));
+
+//builder.Services.AddDefaultIdentity<MVCMessageWallUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<MVCMessageWallContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,6 +27,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
